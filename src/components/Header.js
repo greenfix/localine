bf5ad7ae6035e7a494566258e9cf1b85e9148f6c
Serg <sayrosstes@gmail.com>
2020-01-26 07:21:11 +0300

    Init

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+    <header className="header">
+        <img src="http://www.agrotech1.ru/images/p1_2.jpg"/>
+    </header>
+)
+
+export default Header
---
+++ src/components/Profile.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Profile = () => (
+    <div className="content">
+        <div>
+            <img src="http://www.agrotech1.ru/images/contact.jpg"/>
+        </div>
+        <div>ava + d</div>
+        <div>posts
+            <div>new</div>
+            <div>
+                <div>post 1</div>
+                <div>post 2</div>
+                <div>post 3</div>
+                <div>post 4</div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Profile
